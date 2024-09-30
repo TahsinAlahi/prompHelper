7df93ef85f560a47a8c832cad65cbc19f61d1e87
Tahsin <alahitahsin@gmail.com>
2024-09-30 17:17:54 +0600

    made root-bg styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="main">
+        <div className="root-background">
           <div className="gradient" />
         </div>
 
